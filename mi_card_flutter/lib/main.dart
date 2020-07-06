d6d4f6ace9610fd2bfda70c005bea6359148d56c
Paul Estrada <2215585+pestrada@users.noreply.github.com>
2020-07-06 09:58:25 -0500

    adding ui

--- mi_card_flutter/lib/main.dart
+++ mi_card_flutter/lib/main.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MyApp()
-  );
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -13,33 +11,69 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.teal,
         body: SafeArea(
-          child: Row(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: <Widget>[
-              Container(
-                  height: 100.0,
-                  width: 100.0,
-                  color: Colors.white,
-                  child: Text('Container 1')
+            child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            CircleAvatar(
+              radius: 50.0,
+              backgroundColor: Colors.red,
+              backgroundImage: AssetImage('images/icon_pe.png'),
+            ),
+            Text('Paul Estrada',
+                style: TextStyle(
+                    fontSize: 40.0,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold)),
+            Text(
+              'Software Engineer',
+              style: TextStyle(
+                fontSize: 20.0,
+                letterSpacing: 2.5,
+                color: Colors.teal.shade100,
+                fontWeight: FontWeight.bold,
               ),
-              SizedBox(
-                width: 30.0,
+            ),
+            SizedBox(
+              height: 20.0,
+              width: 150.0,
+              child: Divider(
+                color: Colors.teal.shade100,
               ),
-              Container(
-                width: 100.0,
-                height: 100.0,
-                color: Colors.blue,
-                child: Text('Container 2'),
-              ),
-              Container(
-                width: 100.0,
-                height: 100.0,
-                color: Colors.red,
-                child: Text('Container 3'),
-              ),
-            ],
-          ),
-        ),
+            ),
+            Card(
+              margin: EdgeInsets.symmetric(vertical: 20.0, horizontal: 25.0),
+              child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.phone,
+                      color: Colors.teal,
+                    ),
+                    title: Text(
+                      '+123456789',
+                      style: TextStyle(
+                          color: Colors.teal.shade900, fontSize: 20.0),
+                    ),
+                  )),
+            ),
+            Card(
+              margin: EdgeInsets.symmetric(vertical: 20.0, horizontal: 25.0),
+              child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.mail,
+                      color: Colors.teal,
+                    ),
+                    title: Text(
+                      'contact@email.com',
+                      style: TextStyle(
+                          color: Colors.teal.shade900, fontSize: 20.0),
+                    ),
+                  )),
+            )
+          ],
+        )),
       ),
     );
   }
