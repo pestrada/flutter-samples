6284fcbdf602c3acc78b69eeab45f87dcbb64d56
Paul Estrada <2215585+pestrada@users.noreply.github.com>
2020-07-19 12:14:57 -0500

    refactor

--- xylophone-flutter/lib/main.dart
+++ xylophone-flutter/lib/main.dart
@@ -4,18 +4,37 @@ import 'package:flutter/material.dart';
 void main() => runApp(XylophoneApp());
 
 class XylophoneApp extends StatelessWidget {
+  Expanded addButton(int note, MaterialColor color) {
+    return Expanded(
+      child: FlatButton(
+        color: color,
+        onPressed: () {
+          final player = AudioCache();
+          player.play('note$note.wav');
+        },
+        child: null,
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.black,
         body: SafeArea(
           child: Center(
-            child: FlatButton(
-              onPressed: () {
-                final player = AudioCache();
-                player.play('note1.wav');
-              },
-              child: Text('Click me'),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: <Widget>[
+                addButton(1, Colors.red),
+                addButton(2, Colors.orange),
+                addButton(3, Colors.yellow),
+                addButton(4, Colors.lightGreen),
+                addButton(5, Colors.green),
+                addButton(6, Colors.blue),
+                addButton(7, Colors.purple),
+              ],
             ),
           ),
         ),
